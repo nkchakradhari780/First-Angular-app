514612fd7a954ab82b988c009044fe0959f61e7d
Nitin Chakradhari <154994765+nkchakradhari780@users.noreply.github.com>
2025-06-09 11:40:05 +0530

    implemented two way binding

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, twoWayBinding } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header';
 import { Interpolation } from './components/interpolation/interpolation';
@@ -6,10 +6,11 @@ import { ClassBinding } from './components/class-binding/class-binding';
 import { CommonModule } from '@angular/common';
 import { StyleBinding } from './components/style-binding/style-binding';
 import { EventBinding } from './components/event-binding/event-binding';
+import { TwoWayBinding } from './components/two-way-binding/two-way-binding';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Interpolation, ClassBinding, CommonModule, StyleBinding, EventBinding],
+  imports: [RouterOutlet, Header, Interpolation, ClassBinding, CommonModule, StyleBinding, EventBinding, TwoWayBinding],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/components/two-way-binding/two-way-binding.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-two-way-binding',
+  imports: [FormsModule],
+  templateUrl: './two-way-binding.html',
+  styleUrl: './two-way-binding.css'
+})
+export class TwoWayBinding {
+  name: string = 'Input Box';
+}
